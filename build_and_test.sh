27fc0d5572667d1b3cd492c501be6ee26b785109
Yang Hu <yanghuu531@gmail.com>
2024-11-14 15:51:03 -0500

    done
    Signed-off-by: Yang Hu <yanghuu531@gmail.com>

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,7 +1,21 @@
 cargo build
+
+# Test invalid urls
 target/debug/curl "www.eecg.toronto.edu"
 target/debug/curl "data://www.eecg.toronto.edu"
 target/debug/curl "http//www.eecg.toronto.edu"
 target/debug/curl "https://255.255.255.256"
 target/debug/curl "https://[...1]"
 target/debug/curl "http://127.0.0.1:65536"
+
+# Test server errors
+target/debug/curl "https://example.rs"
+target/debug/curl "https://www.eecg.toronto.edu/~bli/ece1724/assignments/files/lab4.html"
+
+# Test post
+target/debug/curl "https://jsonplaceholder.typicode.com/posts" -d "userId=1&title=Hello World" -X POST
+target/debug/curl "dddd://jsonplaceholder.typicode.com/posts" -d "userId=1&title=Hello World" -X POST
+
+# Test json
+target/debug/curl --json '{"title": "World", "userId": 5}' "https://dummyjson.com/posts/add"
+target/debug/curl --json '{"title": "World"; "userId": 5}' "https://dummyjson.com/posts/add"
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,54 @@
+use std::collections::HashMap;
 use std::io::Read;
 use reqwest::blocking::Client;
+use reqwest::header::CONTENT_TYPE;
 use reqwest::Url;
 use std::net::IpAddr;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
+    /// The URL to request
     url: String,
+
+    /// The HTTP method to use (GET or POST)
+    #[structopt(short = "X", long, default_value = "GET")]
+    method: String,
+
+    /// The data to send in a POST request
+    #[structopt(short = "d", long)]
+    data: Option<String>,
+
+    /// The JSON data to send in a POST request
+    #[structopt(long)]
+    json: Option<String>,
 }
 
 fn main() {
-    // let input_url = std::env::args().nth(1).unwrap_or_else(|| {
-    //     eprintln!("Usage: {} <url>", std::env::args().nth(0).unwrap());
-    //     std::process::exit(1);
-    // });
-
     let args = Opt::from_args();
+
     let input_url = args.url;
+    if args.data.is_some() && args.json.is_some() {
+        eprintln!("Error: The -d and --json options cannot be used together.");
+        std::process::exit(1);
+    }
+    
+    let method = if (args.method.to_uppercase() == "POST" && args.data.is_some()) || args.json.is_some() { "POST" } else { "GET" };
+
     println!("Requesting URL: {}", input_url);
-    println!("Method: GET");
-        
+    println!("Method: {}", method);
+
+    if let Some(ref print_data) = args.data {
+        println!("Data: {}", print_data);
+    }
+
+    if let Some(ref print_data) = args.json {
+        println!("JSON: {}", print_data);
+    }
+
     let splits = input_url.split("://").collect::<Vec<&str>>();
     let parsed_url = Url::parse(&input_url);
-    match &parsed_url {
+    match parsed_url {
         Ok(_) => {
             let base_protocol = splits[0];
             if base_protocol != "http" && base_protocol != "https" {
@@ -47,7 +73,7 @@ fn main() {
                 eprintln!("Error: The URL contains an invalid port number.");
                 std::process::exit(1);
             }
-            eprintln!("Unhandled Error: {}", e);
+            eprintln!("Error: {}", e);
             std::process::exit(1);
         }
     }
@@ -55,10 +81,48 @@ fn main() {
     // URL is ok
     let client = Client::new();
     let parsed_url = parsed_url.unwrap();
-    let response = client.get(parsed_url).send().map_err(|e| e.to_string());
+    let response = match method {
+        "POST" => {
+            if let Some(ref json_data) = args.json {
+                // Check if the JSON data is valid
+                serde_json::from_str::<serde_json::Value>(&json_data).expect("Invalid JSON");
+                client.post(parsed_url).header(CONTENT_TYPE, "application/json").body(json_data.clone()).send().map_err(|e| e.to_string())
+            } else if let Some(ref data) = args.data {
+                let params: HashMap<&str, &str> = data.split('&')
+                .map(|s| {
+                    let mut split = s.splitn(2, '=');
+                    (split.next().unwrap(), split.next().unwrap_or(""))
+                })
+                .collect();
+                client.post(parsed_url).form(&params).send().map_err(|e| e.to_string())
+            } else {
+                eprintln!("Error: The POST method requires data to be sent.");
+                std::process::exit(1);
+            }
+        },
+        _ => client.get(parsed_url).send().map_err(|e| e.to_string()),
+    };
+
     match response {
-        Ok(response) => println!("Response body:\n{}", response.text().unwrap()),
-        Err(e) => eprintln!("Error: {}", e),
+        Ok(response) => {
+            if !response.status().is_success() {
+                eprintln!("Error: Request failed with status code: {}.", response.status().as_u16());
+                std::process::exit(1);
+            }
+            let text = response.text().map_err(|e| e.to_string()).unwrap();
+            if let Ok(json) = serde_json::from_str::<serde_json::Value>(&text) {
+                let sorted_json = serde_json::to_string_pretty(&json).unwrap();
+                println!("Response body (JSON with sorted keys):\n{}", sorted_json);
+            } else {
+                println!("Response body:\n{}", text);
+            }
+        }
+        Err(e) => {
+            if e.contains("error sending request for url") {
+                eprintln!("Error: Unable to connect to the server. Perhaps the network is offline or the server hostname cannot be resolved.");
+                std::process::exit(1);
+            }
+            eprintln!("Error: {}", e);
+        }
     }
-    std::process::exit(0);
 }
